468b209523effb771927f6140ce2fc84f11de53c
Jared Berkowitz <jaredberkowitz2@gmail.com>
2021-12-28 21:25:36 -0500

    about me section mobile changes + button styling

--- src/components/Me.js
+++ src/components/Me.js
@@ -26,10 +26,6 @@ class Me extends Component {
             <span>{sectionName}</span>
           </h1>
           <div className="row center mx-auto mb-5">
-   
-
-            <div className=" center ">
-              <div className="col-md-10">
                 <div className="card ">
                   <div className="card-header">
                     <span
@@ -51,7 +47,7 @@ class Me extends Component {
                     ></span>
                   </div>
                   <div
-                    className="card-body font-trebuchet d-flex text-justify ml-3 mr-3"
+                    className="card-body font-trebuchet d-flex text-justify ml-3 mr-3 "
                     style={{
                       height: "auto",
                       fontSize: "200%",
@@ -62,7 +58,7 @@ class Me extends Component {
 
                         <div className="col-md-5 mt-5 ml-5">
                         <div className="profileStyling">
-                            <span style={{ cursor: "auto" }}>
+                            <span style={{ cursor: "auto", maxWidth:"100%", height:"auto"}}>
                             <img
                                 height="250px"
                                 src={profilepic}
@@ -73,17 +69,17 @@ class Me extends Component {
                         </div>
                         </div>    
     
-                        <ul className="col-md-6 col-sm-4  mt-5 ">
+                        <ul className="mt-5 col-md-6 col-sm-5">
                                 <li className="d-flex align-items-center"><i class="fas fa-gift display-4 text-success mr-3"style={{ fontSize: '24px' }}  ></i><span className={"text-success"}><h3>Birthday:   </h3></span><span><h3 className="ml-3"> 06.03.1996</h3></span></li>
                                 <li className="d-flex align-items-center"><i class="fas fa-map-marker-alt display-4 text-success mr-3"style={{ fontSize: '24px' }}  ></i><span className={"text-success"}><h3>Location:   </h3></span><span><h3 className="ml-3"> Long Island, New York</h3></span></li>
                                 <li className="d-flex align-items-center"><i class="fas fa-book-open display-4 text-success mr-3"style={{ fontSize: '24px' }}  ></i> <span className={"text-success"}><h3>Study:   </h3></span><span><h3 className="ml-3 text-align"> Computer Programming and Information Systems</h3></span></li>
                                 <li className="d-flex align-items-center"><i class="fas fa-envelope display-4 text-success mr-3"style={{ fontSize: '24px' }}  ></i><span className={"text-success"}><h3>Degree:   </h3></span><span><h3 className="ml-3"> Bachelor's</h3></span></li>
                                 <li className="d-flex align-items-center"><i class="fas fa-phone-alt display-4 text-success mr-3"style={{ fontSize: '24px' }}  ></i> <span className={"text-success"}><h3>Cell Phone:   </h3></span><span><h3 className="ml-3">631-827-5359</h3></span></li>
-                                <li className="d-flex align-items-center"><i class="fas fa-gift display-4 text-success mr-3"style={{ fontSize: '24px' }}  ></i> <span className={"text-success"}><h3>Email:   </h3></span><span><h3 className="ml-3"> jaredberkowitz2@gmail.com</h3></span></li>
+                                <li className="d-flex align-items-center text-truncate"><i class="fas fa-gift display-4 text-success mr-3"style={{ fontSize: '24px' }}  ></i> <span className={"text-success"}><h3>Email:   </h3></span><span><h3 className="ml-3"> jaredberkowitz2@gmail.com</h3></span></li>
                                 
                                 <li className="d-flex align-items-center mt-5" >
-                                    <div className="box col-6 col-sm-6"><Button variant="success" size="xl" active className="btn" style={{fontSize: '18px'}}>Download CV</Button></div>
-                                    <div className="box col-6 col-sm-4 ml-5"><Button variant="success" size="xl" active className="btn" style={{fontSize: '24px'}}>Contact</Button></div>
+                                    <div className=" "><Button variant="success" size="xl" active className="btn draw-border col-md-12 col-sm-12 text-responsive" style={{fontSize: '17px'}}>Download CV</Button></div>
+                                    <div className="ml-5"><Button variant="success" size="xl" active className="btn draw-border col-md-12 col-sm-12 text-responsive" style={{fontSize: '17px'}}>Contact</Button></div>
                                 </li>
                         </ul>  
 
@@ -96,8 +92,6 @@ class Me extends Component {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
       </section>
     );
   }
